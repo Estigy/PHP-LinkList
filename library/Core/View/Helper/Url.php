1ccddac57fd66876b6b68e59208617a0daeb12d0
Günther Stadler <guenther.stadler@gmx.net>
2014-01-07 12:18:47 +0100

    Strict Standards: Signature of url() View Helper updated

--- library/Core/View/Helper/Url.php
+++ library/Core/View/Helper/Url.php
@@ -2,7 +2,7 @@
 
 class Core_View_Helper_Url extends Zend_View_Helper_Url
 {
-	public function url($urlOptions = array(), $name = '', $reset = false, $encode = true)
+	public function url(array $urlOptions = array(), $name = null, $reset = false, $encode = true)
 	{
 		if (!array_key_exists('lang', $urlOptions)) {
 			$urlOptions['lang'] = Zend_Registry::get('Zend_Locale')->toString();
